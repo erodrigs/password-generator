9047a4011acb9bd0be556a6d9174750176198674
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-07 17:06:58 -0300

    cria string de caracteres

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&"

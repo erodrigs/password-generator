75f2b799e75553e9c6fdc075881de806a7b99d52
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-07 17:12:01 -0300

    recebe o valor do slider

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&"
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function() {
+  sizePassword.innerHTML = this.value;
+}

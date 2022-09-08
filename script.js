cf88933a01fc9e094f5836f5db96d29238d85695
Erick Rodrigues <erick.prodrigues@ufpe.br>
2022-09-08 07:40:25 -0300

    finalizado

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ sizePassword.innerHTML = sliderElement.value;
 sliderElement.oninput = function() {
   sizePassword.innerHTML = this.value;
 }
+
+function generatePassword() {
+  let pass = "";
+  
+  for(let i = 0, n = charset.length; i < sliderElement.value; i += 1) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword() {
+  alert("Senha copiada com sucesso!");
+  navigator.clipboard.writeText(novaSenha);
+}
